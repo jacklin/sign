a668a99f43aadde6db220df2b2dd60eabd877d61
超级手艺人 <fjxmljc007@163.com>
2019-01-11 17:35:55 +0800

    添加OpensslAes类单元测试与readme文件

--- src/Tool/OpensslAes.php
+++ src/Tool/OpensslAes.php
@@ -4,25 +4,37 @@ namespace Tool;
 /**
  * 基于openssl加解密类
  * (PHP 5 >= 5.3.0, PHP 7)
+ * 
  */
 class OpensslAes {
+	//加密密钥
+	private static $key = 'c32ac3ds0vk1209c';
+	//向量
+	private static $vi = 'd9d71b495a614afc';
+	//加密密码模式
+	private static $cipherMethod = 'AES-128-CBC';
     /**
      * 加密方法
      * BaZhang Platform
      * @Author   Jacklin@shouyiren.net
      * @DateTime 2019-01-10T17:37:23+0800
-     * @param    string                   $string 待加密字符串
+     * @param    string                   $str 待加密字符串
      * @param    string                   $key    密钥(16位字符)
      * @return   string                           加密后的字符串
      */
-    public static function encrypt($string, $key)
+    public static function encrypt($str, $key='', $vi='')
     {
-
-        // openssl_encrypt 加密不同Mcrypt，对秘钥长度要求，超出16加密结果不变
-        $data = openssl_encrypt($string, 'AES-128-ECB', $key, OPENSSL_RAW_DATA);
-
-        $data = strtolower(bin2hex($data));
-
+        if (!extension_loaded('openssl')) {
+            throw new \Exception("You must be install openssl.so. The extension.", 1);
+        }
+    	if (empty($key)) {
+    		$key = self::getKey();
+    	}
+    	if (empty($vi)) {
+    		$vi = self::getVi();
+    	}
+        $data = openssl_encrypt($str, self::getCipherMethod(), $key, OPENSSL_RAW_DATA, $vi);
+        $data = strtolower(bin2hex($data));//二进行制转十六制再转小写
         return $data;
     }
     /**
@@ -34,10 +46,82 @@ class OpensslAes {
      * @param    string                   $key    密钥(16位字符)
      * @return   string                           解密后字符串
      */
-    public static function decrypt($string, $key)
+    public static function decrypt($str, $key='', $vi='')
     {
-        $decrypted = openssl_decrypt(hex2bin($string), 'AES-128-ECB', $key, OPENSSL_RAW_DATA);
-
+        if (!extension_loaded('openssl')) {
+            throw new \Exception("You must be install openssl.so. The extension.", 1);
+        }
+    	if (empty($key)) {
+    		$key = self::getKey();
+    	}
+    	if (empty($vi)) {
+    		$vi = self::getVi();
+    	}
+    	$data = hex2bin($str);//十六进制转二进制
+        $decrypted = openssl_decrypt($data, self::getCipherMethod(), $key, OPENSSL_RAW_DATA, $vi);
         return $decrypted;
+    }
+    /**
+     * 获取加密密钥
+     * BaZhang Platform
+     * @Author   Jacklin@shouyiren.net
+     * @DateTime 2019-01-11T09:40:40+0800
+     * @return   string                  密钥 
+     */
+    public static function getKey(){
+    	return self::$key;
+    }
+    /**
+     * 设置加密密钥
+     * BaZhang Platform
+     * @Author   Jacklin@shouyiren.net
+     * @DateTime 2019-01-11T09:41:10+0800
+     * @param    string                   $key 加密密钥
+     */
+    public static function setKey($key){
+    	self::$key = $key;
+    	return true;
+    }
+    /**
+     * 获取向量
+     * BaZhang Platform
+     * @Author   Jacklin@shouyiren.net
+     * @DateTime 2019-01-11T09:41:52+0800
+     * @return   string                   向量值
+     */
+    public static function getVi(){
+    	return self::$vi;
+    }
+    /**
+     * 设置
+     * BaZhang Platform
+     * @Author   Jacklin@shouyiren.net
+     * @DateTime 2019-01-11T09:42:47+0800
+     * @param    string                   $vi 向量值
+     */
+    public static function setVi($vi){
+    	self::$vi = $vi;
+    	return true;
+    }
+    /**
+     * 获取密码加密方法
+     * BaZhang Platform
+     * @Author   Jacklin@shouyiren.net
+     * @DateTime 2019-01-11T09:41:52+0800
+     * @return   string                   加密密码方法
+     */
+    public static function getCipherMethod(){
+    	return self::$cipherMethod;
+    }
+    /**
+     * 设置密码加密方法
+     * BaZhang Platform
+     * @Author   Jacklin@shouyiren.net
+     * @DateTime 2019-01-11T09:42:47+0800
+     * @param    string                   $vi 加密密码方法
+     */
+    public static function setCipherMethod($cipherMethod){
+    	self::$cipherMethod = $cipherMethod;
+    	return true;
     }
 }
--- test/OpensslAesTest.php
+++ test/OpensslAesTest.php
@@ -13,14 +13,66 @@ class OpensslAesTest extends TestCase
 			[
 				'key' => '1234567890123456',
 				'str' => '13799760258',
-				'encrypt_str' => 'd9d71b495a614afc43dcfc261e91561b',
+				'encrypt_str' => '8c3c40822c9a4d62976064104114c823',
+				'vi' => 'd9d71b495a614afc',
+				'cipher_method' => 'AES-128-CBC',
 			],
-			[
-				'key' => '1234567890123456',
-				'str' => '13799760258',
-				'encrypt_str' => 'd9d71b495a614afc43dcfc261e915612',
-			]
+			// [
+			// 	'key' => '1234567890123456',
+			// 	'str' => '13799760257',
+			// 	'encrypt_str' => 'b34734f690e2ceaa0607dbb7f2ff8751',
+			// ],
+			// [
+			// 	'key' => '1234567890123457',
+			// 	'str' => '13799760257',
+			// 	'encrypt_str' => '76e77fa5bbee01eb97ab9cc9726e2621',
+			// ],
+			// [
+			// 	'key' => '',
+			// 	'str' => '13799760257',
+			// 	'encrypt_str' => '874890d8903820445be1441ebc19c7dc',
+			// ]
 		];
+	}
+
+	/**
+	 * @dataProvider provider
+	 * [testSetKey description]
+	 * BaZhang Platform
+	 * @Author   Jacklin@shouyiren.net
+	 * @DateTime 2019-01-11T10:46:29+0800
+	 * @return   [type]                   [description]
+	 */
+	public function testSetKeyViCipherMethod(){
+		$func_agrs = func_get_args();
+		$key = $func_agrs[0];
+		$str = $func_agrs[1];
+		$estr = $func_agrs[2];
+		$vi = $func_agrs[3];
+		$cipher_method = $func_agrs[4];
+		$this->assertTrue(OpensslAes::setKey($key));
+		$this->assertTrue(OpensslAes::setVi($vi));
+		$this->assertTrue(OpensslAes::setCipherMethod($cipher_method));
+	}
+	/**
+	 * @dataProvider provider
+	 * @depends testSetKeyViCipherMethod
+	 * BaZhang Platform
+	 * @Author   Jacklin@shouyiren.net
+	 * @DateTime 2019-01-11T10:50:43+0800
+	 * @param    [type]                   $key [description]
+	 * @return   [type]                        [description]
+	 */
+	public function testGetKeyViCipherMethod(){
+		$func_agrs = func_get_args();
+		$key = $func_agrs[0];
+		$str = $func_agrs[1];
+		$estr = $func_agrs[2];
+		$vi = $func_agrs[3];
+		$cipher_method = $func_agrs[4];
+		$this->assertEquals($key,OpensslAes::getKey());
+		$this->assertEquals($vi,OpensslAes::getVi());
+		$this->assertEquals($cipher_method,OpensslAes::getCipherMethod());
 	}
 	/**
 	 * @dataProvider provider
