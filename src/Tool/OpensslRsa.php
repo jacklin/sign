06b41da50fad5a41a04f00fbee0a4e37f285f08d
超级手艺人 <fjxmljc007@163.com>
2020-03-05 18:13:09 +0800

    修复编移位错误 添加openssRsa单元测试

--- src/Tool/OpensslRsa.php
+++ src/Tool/OpensslRsa.php
@@ -6,15 +6,55 @@ namespace Tool;
  */
 class OpensslRsa
 {
-	const KEYSIZE = 2048;
+	//生成的密钥位数
+	private static $keySize = 344;
+	//默认pkcs1
+	private static $openn_padding = OPENSSL_PKCS1_PADDING;
 	//公钥文件路径，如：/rsa_public_key.pem
 	private static $publicKeyFilePath;
 	//私钥文件路径，如：/rsa_private_key.pem
 	private static $privateKeyFilePath;
 
-	//设置公钥文件路径
+	/**
+	 * 设置生成的密钥位数
+	 * BaZhang Platform
+	 * @Author   Jacklin@shouyiren.net
+	 * @DateTime 2020-03-05T17:48:47+0800
+	 * @param    integer                  $size 位数
+	 */
+	public static function setKeySize($size=2048){
+		self::$keySize = $size;
+	}
+	/**生成的密钥位数
+	 * 获取
+	 * BaZhang Platform
+	 * @Author   Jacklin@shouyiren.net
+	 * @DateTime 2020-03-05T17:51:14+0800
+	 * @return   [type]                   [description]
+	 */
+	public static function getKeySize(){
+		return self::$keySize;
+	}
+	/**
+	 * 设置浮动
+	 * BaZhang Platform
+	 * @Author   Jacklin@shouyiren.net
+	 * @DateTime 2020-03-05T17:53:39+0800
+	 * @param    [type]                   $padding [description]
+	 */
+	public static function setPadding($padding){
+		self::$openn_padding = $padding;
+	}
+	/**
+	 * 设置公钥文件路径
+	 * BaZhang Platform
+	 * @Author   Jacklin@shouyiren.net
+	 * @DateTime 2020-03-05T17:50:08+0800
+	 * @param    [type]                   $filePath 文件路径
+	 */
 	public static function setPublicKeyFilePath($filePath){
 		self::$publicKeyFilePath = $filePath;
+		return true;
 	}
 	/**
 	 * 获取公钥
@@ -27,12 +67,19 @@ class OpensslRsa
 		if (is_file(self::$publicKeyFilePath)) {
 			return openssl_get_publickey(file_get_contents(self::$publicKeyFilePath));
 		}else{
-			throw new Exception("no such public key file!");
+			throw new \Exception("no such public key file!");
 		}
 	}
-	//设置私钥文件路径
+	/**
+	 * 设置私钥文件路径
+	 * BaZhang Platform
+	 * @Author   Jacklin@shouyiren.net
+	 * @DateTime 2020-03-05T17:50:23+0800
+	 * @param    [type]                   $filePath 文件路径
+	 */
 	public static function setPrivateKeyFilePath($filePath){
 		self::$privateKeyFilePath = $filePath;
+		return true;
 	}
 	/**
 	 * 获取私钥
@@ -41,11 +88,11 @@ class OpensslRsa
 	 * @DateTime 2020-03-05T11:06:11+0800
 	 * @return   string|mixed                   返回private key
 	 */
-	public static function getPrivateKeyFilePath(){
+	public static function getPrivateKey(){
 		if (is_file(self::$privateKeyFilePath)) {
 			return openssl_get_privatekey(file_get_contents(self::$privateKeyFilePath));
 		}else{
-			throw new Exception("no such private key file!");
+			throw new \Exception("no such private key file!");
 		}
 	}
 	/**
@@ -58,12 +105,16 @@ class OpensslRsa
 	 */
 	public static function privateEncrypt($str){
 		$data = "";
-		$dataArray = str_split($str, self::KEYSIZE/8);
+
+		$dataArray = str_split($str, self::$keySize/8-11);
 		$key = self::getPrivateKey();
 		foreach ($dataArray as $value) {
 		    $encryptedTemp = ""; 
-		    openssl_private_encrypt($value,$encryptedTemp,$key,OPENSSL_PKCS1_PADDING);
-		    $data .= $encryptedTemp;
+		    if (openssl_private_encrypt($value,$encryptedTemp,$key,self::$openn_padding)) {
+			    $data .= $encryptedTemp;
+		    }else{
+		    	throw new \Exception("Error openssl_private_encrypt false");
+		    }
 		}
 		openssl_free_key($key);
 		return base64_encode($data);
@@ -80,12 +131,14 @@ class OpensslRsa
 		$decrypted = "";
 		$decodeStr = base64_decode($str);
 		$key = self::getPublicKey();
-		$enArray = str_split($decodeStr, self::KEYSIZE/8);
-
+		$enArray = str_split($decodeStr, self::$keySize/8);
 		foreach ($enArray as $va) {
 		    $decryptedTemp = "";
-		    openssl_public_decrypt($va,$decryptedTemp,$key,OPENSSL_PKCS1_PADDING);
-		    $decrypted .= $decryptedTemp;
+		    if (openssl_public_decrypt($va,$decryptedTemp,$key,self::$openn_padding)) {
+			    $decrypted .= $decryptedTemp;
+		    }else{
+		    	throw new \Exception("Error openssl_public_decrypt false");
+		    }
 		}
 		openssl_free_key($key);
 		return $decrypted;
@@ -101,7 +154,7 @@ class OpensslRsa
 	public static function publicEncrypt($str){
 		$data = "";
 		$key = self::getPublicKey();
-		$dataArray = str_split($str, self::KEYSIZE/8);
+		$dataArray = str_split($str, self::$keySize/8-11);
 		foreach ($dataArray as $value) {
 		   $encryptedTemp = ""; 
 		   openssl_public_encrypt($value,$encryptedTemp,$key,OPENSSL_PKCS1_PADDING);
@@ -122,7 +175,7 @@ class OpensslRsa
 		$decrypted = "";
 		$decodeStr = base64_decode($str);
 		$key = self::getPrivateKey();
-		$enArray = str_split($decodeStr, self::KEYSIZE/8);
+		$enArray = str_split($decodeStr, self::$keySize/8);
 
 		foreach ($enArray as $va) {
 		    $decryptedTemp = "";
---
+++ test/OpensslRsaTest.php
@@ -0,0 +1,79 @@
+<?php 
+require_once(__dir__."/../src/Tool/OpensslRsa.php");
+
+use PHPUnit\Framework\TestCase;
+use Tool\OpensslRsa; 
+/**
+ * 
+ */
+class OpensslRsaTest extends TestCase
+{
+	public function provider(){
+		return [
+			[
+				'public_key' => './opensslkey/rsa_public_key.pem',
+				'private_key' => './opensslkey/rsa_private_key_pkcs8.pem',
+				'str' => '88eab79690b2c9c1c39c0cc523ad1118',
+			],
+			// [
+			// 	'key' => '1234567890123456',
+			// 	'str' => '13799760257',
+			// 	'encrypt_str' => 'b34734f690e2ceaa0607dbb7f2ff8751',
+			// ],
+			// [
+			// 	'key' => '1234567890123457',
+			// 	'str' => '13799760257',
+			// 	'encrypt_str' => '76e77fa5bbee01eb97ab9cc9726e2621',
+			// ],
+			// [
+			// 	'key' => '',
+			// 	'str' => '13799760257',
+			// 	'encrypt_str' => '874890d8903820445be1441ebc19c7dc',
+			// ]
+		];
+	}
+
+	/**
+	 * @dataProvider provider
+	 * [testSetKey description]
+	 * BaZhang Platform
+	 * @Author   Jacklin@shouyiren.net
+	 * @DateTime 2019-01-11T10:46:29+0800
+	 * @return   [type]                   [description]
+	 */
+	public function testSetKey(){
+		$func_agrs = func_get_args();
+		$public_key_file = $func_agrs[0];
+		$private_key_file = $func_agrs[1];
+		$this->assertTrue(OpensslRsa::setPublicKeyFilePath($public_key_file));
+		$this->assertTrue(OpensslRsa::setPrivateKeyFilePath($private_key_file));
+	}
+	/**
+	 * @dataProvider provider
+	 * BaZhang Platform
+	 * @Author   Jacklin@shouyiren.net
+	 * @DateTime 2019-01-10T18:47:00+0800
+	 * @return   [type]                   [description]
+	 */
+	public function testEncrypt(){
+		$func_agrs = func_get_args();
+		$func_agrs = func_get_args();
+		$public_key_file = $func_agrs[0];
+		$private_key_file = $func_agrs[1];
+		$str = $func_agrs[2];
+		OpensslRsa::setPublicKeyFilePath($public_key_file);
+		OpensslRsa::setPrivateKeyFilePath($private_key_file);
+		echo PHP_EOL;
+		echo $str.PHP_EOL;
+		$encrypt_str = OpensslRsa::privateEncrypt($str);
+		echo $encrypt_str.PHP_EOL;
+		$descrypt_str = OpensslRsa::publicDescrypt($encrypt_str);
+		echo $descrypt_str.PHP_EOL;
+		$this->assertEquals($str,$descrypt_str);
+		$encrypt_str = OpensslRsa::publicEncrypt($str);
+		echo $encrypt_str.PHP_EOL;
+		$descrypt_str = OpensslRsa::privateDescrypt($encrypt_str);
+		echo $descrypt_str.PHP_EOL;
+		$this->assertEquals($str,$descrypt_str);
+	}
+}
